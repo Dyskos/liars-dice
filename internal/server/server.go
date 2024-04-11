b6bc53b3e19f379a50c15d806c26857c167d01c2
Christy X. Winslett <winslett.christian@gmail.com>
2024-04-11 18:02:11 -0500

    Listen for new connections

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,8 +2,30 @@ package server
 
 import (
 	"fmt"
+	"net"
+	"os"
 )
 
 func Run() {
-	fmt.Println("Hello, server!")
+	port := "10150"
+	// TODO: use configurable port
+	ln, err := net.Listen("tcp", ":" + port)
+	if err != nil {
+		fmt.Printf("! %s\r\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("[*] Listening on: %v\r\n", ln.Addr())
+
+	for {
+		cn, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("[!] %s\r\n", err)
+		}
+		go handleConnection(cn)
+	}
+}
+
+func handleConnection(cn net.Conn) {
+	fmt.Printf("[*] connection established: %v\r\n", cn)
+	return
 }
